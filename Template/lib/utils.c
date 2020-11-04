be86c0de0f3633abf8ea5b2f01119f385ec335b2
awalx003new <rize.kat11@gmail.com>
2020-11-03 19:05:05 -0600

    debug with group

--- Template/lib/utils.c
+++ Template/lib/utils.c
@@ -1,7 +1,12 @@
 #include "utils.h"
+#include "mapper.h"//to use mapperID variable
+#include <stdbool.h>//to use boolean variable
 #include <sys/msg.h>//-->in order to use message queues
 #define PERM 0666//--> user, group, and others each have only read and write permissions
 #define ERROR 0//for errors in the code
+#define LESS_THAN_CHUNKSIZE 1
+#define EQUAL_CHUNKSIZE 2
+#define MORE_THAN_CHUNKSIZE 3
 
 char *getChunkData(int mapperID)
 
@@ -11,7 +16,7 @@ char *getChunkData(int mapperID)
 
   //declare the same key as in the "sendchunkdata". To be used again to open the message queue.
 
-  key_t key = ftok("project",2); // convert the pathname "key" and the reducer identifier to a System V IPC
+  key_t key = ftok(".",5331326); // convert the pathname "key" and the reducer identifier to a System V IPC
 
   //declare a variable msg of type struct msgBuffer to represent the chunk4
 
@@ -23,102 +28,165 @@ char *getChunkData(int mapperID)
 
   if(mid == -1)
   {
-    return -1; //error handling. return -1 and set errno
+    exit(ERROR); //error handling. return -1 and set errno
   }
+  
+  //struct msgBuffer *ptr = malloc(MSGSIZE);
 
-  msgrcv(mid, &msg, sizeof(msg.msgText), mapperID, 0); //receive data from the master who was supposed to send a specific mapperID
-
-
+  int checkStatus = msgrcv(mid, &msg, sizeof(msg.msgText), mapperID, 0); //receive data from the master who was supposed to send a specific mapperID
+  if(checkStatus == -1){
+  	perror("Could not receive the data");
+  	exit(ERROR);
+  }else{
+  	//free(ptr);
+  	char *v = msg.msgText;
+  	return v;
+  }
+  
 }
+
+//helper function for sendChunkData()
+int compareToChunkSize(int currentChunkSize, int characterCount, char *wholeString, int nMappers, struct msgBuffer msg){
+    //int ok;
+    //int msgid
+    
+    key_t key;
+    int msgid, ok;
+    
+    //generate unique key
+    key = ftok(".", 5331326);//--> use your x500 as the second argument of ftok(): a TA talks about this in the most recent Canvas announcement
+
+    //creates a message queue
+    msgid = msgget(key, PERM | IPC_CREAT);
+    
+    if(currentChunkSize < chunkSize){
+        strcat(msg.msgText,wholeString);//--> concatenate wholeString to msg.text (which is chunk data thus far)
+        strcat(msg.msgText," ");//--> whitespace character
+        memset(wholeString, '\0', chunkSize);//--> so reset "wholeString"
+        msgctl(msgid, IPC_RMID, NULL);//close the message queue before I leave this function
+        return LESS_THAN_CHUNKSIZE;
+    }else if(currentChunkSize > chunkSize){
+    	bool yes = true;
+    	int i;
+    	for(i = 0; i < MSGSIZE; i++){
+    		if(msg.msgText[i] != '\0'){
+    			yes = false;//I did not call memset on msg.msgText
+    		}
+    	}
+        if(true){//msg.text is just null terminators
+            perror("Word in chunk is too long - cannot be sent without being separated.");
+            exit(ERROR);
+        }
+        //--> then DO NOT concatenate whole string to "msg.text"
+        ok = msgsnd(msgid, (void *) &msg, MSGSIZE, mapperID);
+        if(ok == -1){
+            perror("Could not send the data");
+            exit(ERROR);
+        }
+        
+        
+        if(mapperID < nMappers){
+            mapperID++;
+        }
+        msgctl(msgid, IPC_RMID, NULL);//close the message queue before I leave this function
+        return MORE_THAN_CHUNKSIZE;
+    }else{
+        strcat(msg.msgText,wholeString);//--> concatenate wholeString to msg.text (which is chunk data thus far)
+        strcat(msg.msgText," ");//--> whitespace character
+        ok = msgsnd(msgid, (void *) &msg, MSGSIZE, mapperID);
+        if(ok == -1){
+            perror("Could not send the data");
+            exit(ERROR);
+        }
+        if(mapperID < nMappers){
+            mapperID++;
+        }
+        //--> reset characterCount, currentChunkSize, and "msg.text" (chunk data)
+        characterCount = 0;
+        currentChunkSize = 0;
+        memset(msg.msgText, '\0', MSGSIZE);//--> reset "msg.text" (chunk data)
+        memset(wholeString, '\0', chunkSize);//--> so reset "wholeString"
+        msgctl(msgid, IPC_RMID, NULL);//close the message queue before I leave this function
+        return EQUAL_CHUNKSIZE;
+    }
+}
+
+
+
+
+
 // sends chunks of size 1024 to the mappers in RR fashion
 void sendChunkData(char *inputFile, int nMappers)
 
 {
+	key_t key;
+	int msgid, ok, ok2;
+	struct msgBuffer msg;
+	char checkString;//--> I declared this here because I do not believe you declared it somewhere
+	int characterCount = 0;//--> use this to see if a word is shared between the end of one chunk and the beginning of the next chunk
+    	char wholeString[chunkSize];//--> temporary storage
+    	memset(wholeString, '\0', chunkSize);//--> to make sure it is always null-terminated
+    	int currentChunkSize = 0;
 
-  key_t key;
-  int msgid, ok, ok2;
-  message msg;
-  int checkString;//--> I declared this here because I do not believe you declared it somewhere
-  int characterCount = 0;//--> use this to see if a word is shared between the end of one chunk and the beginning of the next chunk
-  char wholeString[chunkSize];//--> temporary storage
-  memset(wholeString, '\0', chunkSize);//--> to make sure it is always null-terminated
-  int currentChunkSize = 0;
+	//generate unique key
+	key = ftok(".", 5331326);//--> use your x500 as the second argument of ftok(): a TA talks about this in the most recent Canvas announcement
 
-  //generate unique key
-  key = ftok("project", 2);//--> use your x500 as the second argument of ftok(): a TA talks about this in the most recent Canvas announcement
+	//creates a message queue
+	msgid = msgget(key, PERM | IPC_CREAT);
 
-  //creates a message queue
-  msgid = msgget(key, PERM | IPC_CREAT);
+	FILE* file = fopen (inputFile, "r");
+	//char line[chunkSize];//--> chunkSize Macro is defined in "utils.h"
 
-  FILE* file = fopen (inputFile, "r");
-  char line[chunkSize];//--> chunkSize Macro is defined in "utils.h"
+	//--> error checking for fopen() --> see if the file pointer points to anything non-NULL
+	if(file == NULL){
+        	perror("This file could not be opened");
+        	exit(ERROR);
+    	}
 
-  //--> error checking for fopen() --> see if the file pointer points to anything non-NULL
-  if(file == NULL){
-    printf("This file could not be opened");
-    exit(ERROR);
-  }
+    	mapperID = 1;
 
-  mapperID = 0;
+	//construct chunks of 1024 bytes each and send
+	//each chunk to a mapper
+	checkString = fgetc (file);
+	while (checkString != EOF){//--> check to see if the pointer is not at the end-of-file
+		characterCount++;//--> a character has been read for current chunk
+		if(checkString != ' '){
+            		strncat(wholeString,&checkString, 1);//--> concatenate to wholeString - size of one character is 1 byte
+		}else{//--> checkString != ' '
+            //--> if at this point, we read one whole string in inputFile
+            //--> add size of a whole string
+            currentChunkSize += (characterCount-1);//--> the "-1" is to account for the space
+            int x = compareToChunkSize(currentChunkSize, characterCount, wholeString, nMappers, msg);
+            if(x == MORE_THAN_CHUNKSIZE){
+                memset(msg.msgText, '\0', MSGSIZE);//--> reset "line" (chunk)
+                compareToChunkSize((characterCount-1), characterCount, wholeString, nMappers, msg);
+            }
+		}
+	    checkString = fgetc (file);//update file pointer
+	    
+	}
 
-  //construct chunks of 1024 bytes each and send
-  //each chunk to a mapper
-  while ((checkString = fgetc (file)) != EOF){//--> check to see if the pointer is not at the end-of-file
-    characterCount++;//--> a character has been read for current chunk
-    if(checkString != ' '){
-      strcat(wholeString,checkString);//--> concatenate to wholeString
-    }else{//--> checkString != ' '
-      //--> if at this point, we read one whole string in inputFile
+	//send end message to mappers
+	for (int i=0; i<nMappers; i++){
+		msg.msgType = i;//--> use mapperID (i) as the tag
+		memset(msg.msgText, '\0', MSGSIZE);
+		sprintf(msg.msgText, "END");
+		ok2 = msgsnd(msgid, (void *)&msg, MSGSIZE, i);
+	}
+	/*
 
-      //--> add size of a whole string
-      currentChunkSize += (characterCount-1);//--> the "-1" is to account for the space
-      if(currentChunkSize <= chunkSize){
-        strcat(line,wholeString);//--> concatenate wholeString to line (which is chunk thus far)
-        strcat(line,checkString);//--> whitespace character
-        if(currentChunkSize == chunkSize){
-          ok = msgsnd(msgid, (void *) &line, mapperID)
-          if(mapperID < nMappers){
-            mapperID++;
-          }
-          //--> reset characterCount, currentChunkSize, and "line" (chunk)
-          characterCount = 0;
-          currentChunkSize = 0;
-          memset(line, '\0', chunkSize);//--> reset "line" (chunk)
-        }
-        memset(wholeString, '\0', chunkSize);//--> so reset "wholeString"
-      }else if(currentChunkSize > chunkSize){
-        //--> then DO NOT concatenate whole string to "line"
-        ok = msgsnd(msgid, (void *) &line, mapperID)
-        if(mapperID < nMappers){
-          mapperID++;
-        }
-        //--> reset currentChunkSize (because we are making a new chunk), and reset "line" (chunk)
-        currentChunkSize = 0;
-        memset(line, '\0', chunkSize);//--> reset "line" (chunk)
+    //--> is this for the ACK??
+	for (int i =0; i<nMappers; i++){
+		wait(msgid);
+	}
+	*/
 
-        currentChunkSize += (characterCount-1);//--> at this point, I want to go back to line 53 --> I'm not sure how to implement this
-      }
-    }
-  }
+	//close message queue
+	msgctl(msgid, IPC_RMID, NULL);
 
-  //send end message to mappers
-  for (int i=0; i<nMappers; i++){
-    msg.mtype = i+1;//--> use mapperID (i) as the tag
-    memset(msg.mtext, '\0', MSGSIZE);
-    sprintf(msg.mtext, "END");
-    ok2 = msgsnd(msgid, (void *)&msg, MSGSIZE, i);
-  }
-
-  //--> is this for the ACK??
-  for (int i =0; i<nMappers; i++){
-    wait(msgid);
-  }
-
-  //close message queue
-  msgctl(msgid, IPC_RMID, NULL);
-
-  fclose(file);
+	fclose(file);
 }
+
 
 // hash function to divide the list of word.txt files across reducers
 //http://www.cse.yorku.ca/~oz/hash.html
@@ -133,23 +201,23 @@ int hashFunction(char* key, int reducers){
 }
 
 int getInterData(char *key, int reducerID) {
-    message one;
+    struct msgBuffer one;
     int messageID;
     key_t key1;
 
-    key1 = ftok(key, awalx003);//file path is the "key"
+    key1 = ftok(".", 5331326);
 
     //open message queue
-    messageID = msget (key1, PERM | IPC_CREAT);
+    messageID = msgget (key1, PERM | IPC_CREAT);
 
-    key1 = msgrcv (messageID, (void *)&one, sizeof (one.mtext), reducerID, 0);
+    key1 = msgrcv (messageID, (void *)&one, sizeof (one.msgText), reducerID, 0);
+    //TODO: Error checking for
 
     //check for END message
-    if(one.mtext == "END"){
-        *key = key;
+    if(one.msgText == "END"){
         return 0;//done reading data
     }else{
-        *key = key;
+        strcpy(key, one.msgText);//copy m.msgText into key
         return 1;//more data to come
     }
 }
@@ -187,8 +255,8 @@ void shuffle(int nMappers, int nReducers)
 
     if (dir == NULL)
     {
-      printf("The path passed is invalid");
-      return -1;
+      //perror("The path passed is invalid");
+      //exit(ERROR);
     }
 
 
@@ -209,7 +277,8 @@ void shuffle(int nMappers, int nReducers)
 
         if (a == -1)
         {
-          return -1;     //error handling for msgsnd. return -1 if cannot send the message
+          //perror("Couldn't send the message");
+          //exit(ERROR);     //error handling for msgsnd. return -1 if cannot send the message
         }
 
       }
@@ -232,21 +301,11 @@ void shuffle(int nMappers, int nReducers)
 
     if(b==-1)
     {
-      return -1; //error handling return -1 if cannot send message
+      //perror("Couldn't send the message");
+      //exit(ERROR); //error handling return -1 if cannot send message
 
     }
   }
-
-  //wait for ACK from the reducers for END notification
-
-  for (int i=0; i<nReducers; i++)
-  {
-    wait(mid);
-  }
-
-  //close the message queue
-
-  msgctl(mid, IPC_RMID, NULL);
 
 }
 
